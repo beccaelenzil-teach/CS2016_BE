26dd4ae5e01b42c24a6961b896ab07ea8c9e4f19
becca <beccastoloff@berkeley.edu>
2016-10-25 09:07:45 -0700

    recursive fdbk

---
+++ ProblemSet4_Tests.py
@@ -0,0 +1,19 @@
+print 'Total Score: '
+print 'Notes: '
+print "------------------------------------- "
+print 'Problem 1) Recursive Drawing '
+print 'Notes: '
+print " "
+print "------------------------------------- "
+print 'Problem 2) Recursive Functions '
+print 'Notes: '
+print " "
+print '# Fibonnaci Tests'
+print "fib(5) == 5 = ",fib(5)," : ", 5 == fib(5)
+print "fib(11) == 89 = ",fib(11)," : ", 89 == fib(11)
+print "fibIter(5) == 5 = ",fibIter(5)," : ", 5 == fibIter(5)
+print "fibIter(11) == 89 = ",fibIter(11)," : ", 89 == fibIter(11)
+print " "
+print '# List Reverse Tests'
+print "listReverse([1,2,3,4]) == [4,3,2,1] = ",listReverse([1,2,3,4])," : ",listReverse([1,2,3,4]) == [4,3,2,1]
+print "listReverseIter([1,2,3,4]) == [4,3,2,1] = ",listReverseIter([1,2,3,4])," : ",listReverseIter([1,2,3,4]) == [4,3,2,1]
